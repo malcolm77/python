23f57824a4f82cf7506698196404163d60f00ffd
malcolm chalmers <malcolmchalmers@hotmail.com>
2024-09-15 15:44:31 +1000

    added youtube code

---
+++ youtube/search3.py
@@ -0,0 +1,21 @@
+from youtubesearchpython import VideosSearch
+
+videosSearch = VideosSearch('Highlights NFL 2024', limit = 20)
+
+print('<h1>NFL Highlights</h1>', file=open('/var/www/html/nfl.html', 'w'))
+
+for i in range(10):
+    # print(videosSearch.result()['result'][i]['link'])
+
+    # title = json['entry']['title']['$t']
+    # author = json['entry']['author'][0]['name']
+    # print ("id:%s\nauthor:%s\ntitle:%s" % (id, author, title))
+
+    title = videosSearch.result()['result'][i]['title'] 
+    link = videosSearch.result()['result'][i]['link'] 
+    posted = videosSearch.result()['result'][i]['publishedTime'] 
+    print ( "%s %s ( %s )" % (title,link,posted) )
+
+    with open('/var/www/html/nfl.html', 'a') as f:
+      print ( "<a href='%s'> %s ( %s ) </a><br>" % (link,title,posted), file=f )
+      # print('Filename:', filename, file=f)  # Python 3.x
